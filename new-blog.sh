8aa8325e3edd084dc2c2b33a7079123aeddebcc6
Lexer747 <Lexer747@gmail.com>
2025-09-06 11:30:03 +0100

    New blog script

---
+++ new-blog.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+baseURL="https://lexer747.github.io/blogs/DATE/"
+
+ColorOff='\033[0m'
+Red='\033[0;31m'
+Green='\033[0;32m'
+
+echo "Please enter a folder name, this is not the same as the title, this will be the URL used"
+read foldername
+
+echo -e "You entered ${Green}$foldername${ColorOff}"
+echo -e "This will result in ${Green}$baseURL$foldername.html${ColorOff}"
+echo -e "Is this correct? (y/N)"
+
+read yes
+if ! [[ $yes == "y" || $yes == "Y" || $yes == "yes" || $yes == "YES" ]]; then
+    exit 0
+fi
+
+ROOT=$(git rev-parse --show-toplevel)
+dir="$ROOT/content/pages/blogs/$foldername"
+
+if [ -d "$dir" ]; then
+    echo -e "${Red}Folder already found, cannot create.${ColorOff}"
+    exit 1
+fi
+
+mkdir "$dir"
+mkdir "$dir"/images
+
+echo "" > "$dir"/images/.gitkeep
+echo "TODO :)" > "$dir"/content.md
+echo "" > "$dir"/notes.txt
+echo "UNKNOWN" > "$dir"/published.content
+echo "" > "$dir"/revisions.content
+echo "UNKNOWN Title" > "$dir"/title.content
